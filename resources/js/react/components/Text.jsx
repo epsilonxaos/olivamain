6d7a7ec92afcc91ee2874710d3e6453697adaecf
Jesus Glez <jesusgleztr94@gmail.com>
2024-04-20 11:14:47 -0600

    feat(Web): :sparkles: Control de textos

---
+++ resources/js/react/components/Text.jsx
@@ -0,0 +1,59 @@
+import { twMerge } from "tailwind-merge";
+
+const Text = ({ children, className }) => {
+    return (
+        <p
+            className={twMerge(
+                "font-intervogueReg leading-6 text-inherit",
+                className,
+            )}
+        >
+            {children}
+        </p>
+    );
+};
+
+const Main = ({ children, className }) => {
+    return (
+        <h1
+            className={twMerge(
+                "text-center font-olivaSans text-2xl uppercase leading-7 tracking-[4px] text-inherit",
+                className,
+            )}
+        >
+            {children}
+        </h1>
+    );
+};
+
+const Title = ({ children, className }) => {
+    return (
+        <h2
+            className={twMerge(
+                "text-center font-olivaSans text-2xl uppercase leading-7 tracking-[4px] text-inherit",
+                className,
+            )}
+        >
+            {children}
+        </h2>
+    );
+};
+
+const Subtitle = ({ children, className }) => {
+    return (
+        <h3
+            className={twMerge(
+                "text-center font-olivaSans uppercase leading-6 tracking-[1.6px] text-inherit",
+                className,
+            )}
+        >
+            {children}
+        </h3>
+    );
+};
+
+Text.Main = Main;
+Text.Title = Title;
+Text.Subtitle = Subtitle;
+
+export default Text;
