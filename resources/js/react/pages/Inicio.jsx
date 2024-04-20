ab7d9e90516f99851cb2b5e6dda88c6d7244137f
Jesus Glez <jesusgleztr94@gmail.com>
2024-04-20 11:16:31 -0600

    refactor(Web): :recycle: Agregando los componentes de seccion

--- resources/js/react/pages/Inicio.jsx
+++ resources/js/react/pages/Inicio.jsx
@@ -1,5 +1,19 @@
+import Bienvenida from "../components/Inicio/Bienvenida";
+import Contacto from "../components/Inicio/Contacto";
+import Instagram from "../components/Inicio/Instagram";
+import Sucursales from "../components/Inicio/Sucursales";
+import Video from "../components/Inicio/Video";
+
 const Inicio = () => {
-    return <section></section>;
+    return (
+        <section className="py-8 text-black">
+            <Video />
+            <Bienvenida />
+            <Sucursales />
+            <Instagram />
+            <Contacto />
+        </section>
+    );
 };
 
 export default Inicio;
