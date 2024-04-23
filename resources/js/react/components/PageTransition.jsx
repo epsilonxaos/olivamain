95247b7a2967f735322f5a9b670af4f9d2b17fd2
Jesus Glez <jesusgleztr94@gmail.com>
2024-04-22 21:05:36 -0600

    refactor(Web): :lipstick: Cambiando tiempos en las transiciones

--- resources/js/react/components/PageTransition.jsx
+++ resources/js/react/components/PageTransition.jsx
@@ -30,14 +30,14 @@ const PageTransition = ({ children }) => {
                 initial={{ scaleY: 0 }}
                 animate={{ scaleY: 0 }}
                 exit={{ scaleY: 1 }}
-                transition={{ duration: 0.6, ease: [0.22, 1, 0.36, 1] }}
+                transition={{ duration: 0.3, ease: [0.22, 1, 0.36, 1] }}
             />
             <motion.div
                 style={slideOut}
                 initial={{ scaleY: 1 }}
                 animate={{ scaleY: 0 }}
                 exit={{ scaleY: 0 }}
-                transition={{ duration: 0.6, ease: [0.22, 1, 0.36, 1] }}
+                transition={{ duration: 1.5, ease: [0.22, 1, 0.36, 1] }}
             />
         </>
     );
