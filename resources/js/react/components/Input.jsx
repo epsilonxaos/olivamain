562232565e13907454f8b79d6f6dff0a236fa440
Jesus Glez <jesusgleztr94@gmail.com>
2024-04-22 21:05:49 -0600

    feat(Web): :sparkles: Componente para inputs

---
+++ resources/js/react/components/Input.jsx
@@ -0,0 +1,45 @@
+import { twMerge } from "tailwind-merge";
+
+export default function Input({
+    className,
+    name,
+    type = "text",
+    label,
+    register,
+    validate = false,
+    rules,
+    validateError = "",
+    validateErrorMessage = "",
+}) {
+    return (
+        <div className={twMerge("group relative z-0 mb-7 w-full", className)}>
+            <input
+                {...(type && { type })}
+                name={name}
+                id={name}
+                className="peer block w-full appearance-none rounded-xl border-2 border-white bg-transparent px-4 py-2.5 font-intervogueReg text-sm text-inherit focus:border-white focus:outline-none focus:ring-0"
+                placeholder=" "
+                {...(validate && register(name, rules))}
+                {...(!validate && { name })}
+                required
+            />
+
+            {validateError && (
+                <span className=" text-xs text-red-400">
+                    {validateErrorMessage}
+                </span>
+            )}
+
+            <label
+                htmlFor={name}
+                className={`absolute left-4 top-2 -z-10 origin-[0] -translate-y-7 scale-75 font-intervogueReg text-base text-inherit duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:start-4 peer-focus:-translate-y-7 peer-focus:scale-75 peer-focus:font-medium peer-focus:text-white rtl:peer-focus:left-4 rtl:peer-focus:translate-x-1/4 ${
+                    validateError &&
+                    "border-2 border-red-300 bg-red-100 text-black"
+                }`}
+            >
+                {label}
+                {validate && <span className="font-bold text-red-700">*</span>}
+            </label>
+        </div>
+    );
+}
