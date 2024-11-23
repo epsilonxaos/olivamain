646d9ef669af893fec195013020cb6807ba7e590
Epsilonxaos <angelotr12@hotmail.com>
2024-11-23 15:48:07 -0600

    refactor(home): :lipstick: actualizacion de separadores

--- resources/js/react/pages/home/Branches.jsx
+++ resources/js/react/pages/home/Branches.jsx
@@ -24,6 +24,7 @@ const Branches = () => {
 
 			<main>
 				<BranchSection />
+				<BranchSection />
 			</main>
 		</section>
 	)
@@ -33,65 +34,67 @@ const BranchSection = () => {
 	const { isDesktop } = useResponsiveMediaQuery()
 
 	return (
-		<Container className='border-b border-b-black py-40'>
-			<div className='grid grid-cols-1 items-center gap-8 lg:grid-cols-2 lg:gap-16 2xl:gap-24'>
-				<div>
-					<div className='flex items-center justify-center lg:mb-6 lg:justify-start'>
-						<img
-							src={ImgLogoPlaceholder}
-							alt='Logo'
-							className='size-[90px] lg:size-[130px] xl:size-[190px]'
-						/>
+		<div className='border-b border-b-black'>
+			<Container className='py-40'>
+				<div className='grid grid-cols-1 items-center gap-8 lg:grid-cols-2 lg:gap-16 2xl:gap-24'>
+					<div>
+						<div className='flex items-center justify-center lg:mb-6 lg:justify-start'>
+							<img
+								src={ImgLogoPlaceholder}
+								alt='Logo'
+								className='size-[90px] lg:size-[130px] xl:size-[190px]'
+							/>
 
-						<main className='pl-6 xl:pl-10'>
-							<Text.Title className='mb-1 tracking-[1.6px]'>Horarios</Text.Title>
-							<Text className='mb-0'>
-								Lunes a Miércoles 1:00 pm a 11:00 pm <br />
-								Jueves a Sábados 1:00 pm a 12:00 am <br />
-								Domingo 1:00 pm a 10:00 pm
-							</Text>
-						</main>
-					</div>
+							<main className='pl-6 xl:pl-10'>
+								<Text.Title className='mb-1 tracking-[1.6px]'>Horarios</Text.Title>
+								<Text className='mb-0'>
+									Lunes a Miércoles 1:00 pm a 11:00 pm <br />
+									Jueves a Sábados 1:00 pm a 12:00 am <br />
+									Domingo 1:00 pm a 10:00 pm
+								</Text>
+							</main>
+						</div>
 
-					<div className='hidden border-y border-y-black py-8 lg:grid lg:grid-cols-4 lg:gap-5'>
-						<Button className='px-2'>Delivery</Button>
-						<Button className='px-2'>Reserva</Button>
-						<Button className='px-2'>Menú</Button>
-						<Button className='px-2'>Conoce más</Button>
-					</div>
+						<div className='hidden border-y border-y-black py-8 lg:grid lg:grid-cols-4 lg:gap-5'>
+							<Button className='px-2'>Delivery</Button>
+							<Button className='px-2'>Reserva</Button>
+							<Button className='px-2'>Menú</Button>
+							<Button className='px-2'>Conoce más</Button>
+						</div>
 
-					<Location
-						content='Ubicado en el Malecón de La Paz Baja California Sur.'
-						className='hidden items-center py-8 lg:flex'
-					/>
-				</div>
-
-				<div className='border border-black p-2 lg:p-4'>
-					<video
-						autoPlay
-						loop
-						muted
-						className='mx-auto h-auto min-h-[230px] w-full object-cover lg:h-[590px] lg:max-w-[750px]'
-						poster={ImgPosterPlaceholder}>
-						<source
-							src={VideoPlaceholder}
-							type='video/mp4'
-						/>
-						Your browser does not support the video tag.
-					</video>
-				</div>
-
-				{!isDesktop && (
-					<div className='lg:hidden'>
 						<Location
 							content='Ubicado en el Malecón de La Paz Baja California Sur.'
-							className='mb-4 flex items-center'
+							className='hidden items-center py-8 lg:flex'
 						/>
-						<Button className='w-full px-2'>Conoce más</Button>
 					</div>
-				)}
-			</div>
-		</Container>
+
+					<div className='border border-black p-2 lg:p-4'>
+						<video
+							autoPlay
+							loop
+							muted
+							className='mx-auto h-auto min-h-[230px] w-full object-cover lg:h-[590px] lg:max-w-[750px]'
+							poster={ImgPosterPlaceholder}>
+							<source
+								src={VideoPlaceholder}
+								type='video/mp4'
+							/>
+							Your browser does not support the video tag.
+						</video>
+					</div>
+
+					{!isDesktop && (
+						<div className='lg:hidden'>
+							<Location
+								content='Ubicado en el Malecón de La Paz Baja California Sur.'
+								className='mb-4 flex items-center'
+							/>
+							<Button className='w-full px-2'>Conoce más</Button>
+						</div>
+					)}
+				</div>
+			</Container>
+		</div>
 	)
 }
 
