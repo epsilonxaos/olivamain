8220d791a802d487acd4702a9399ad22bf465d5c
Epsilonxaos <angelotr12@hotmail.com>
2024-12-23 23:45:57 -0600

    refactor(SwipeGallery): :recycle: Integrando galería desde el contexto de la aplicación y eliminando imágenes estáticas

--- resources/js/react/pages/home/SwipeGallery.jsx
+++ resources/js/react/pages/home/SwipeGallery.jsx
@@ -1,26 +1,27 @@
+import { useContext } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 import Container from '../../components/Container'
-
-const images = [
-	'https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg',
-	'https://images.pexels.com/photos/262978/pexels-photo-262978.jpeg',
-	'https://images.pexels.com/photos/1307698/pexels-photo-1307698.jpeg',
-]
+import AppContext from '../../contexts/AppContext'
 
 const SwipeGallery = () => {
+	const { state } = useContext(AppContext)
+	const { galeria } = state
+
 	return (
 		<Container className='max-w-none p-0 xl:p-0'>
 			<Swiper slidesPerView={1}>
-				{images.map((image, index) => (
-					<SwiperSlide key={'main-gallery-' + index}>
-						<img
-							className='h-[400px] max-h-[800px] w-full object-cover lg:h-[85svh]'
-							src={image}
-							alt='Main gallery'
-						/>
-					</SwiperSlide>
-				))}
+				{galeria
+					.filter(({ section }) => section === 'principal')
+					.map(({ cover, id }) => (
+						<SwiperSlide key={'main-gallery-' + id}>
+							<img
+								className='h-[400px] max-h-[800px] w-full object-cover lg:h-[85svh]'
+								src={cover}
+								alt='Main gallery'
+							/>
+						</SwiperSlide>
+					))}
 			</Swiper>
 		</Container>
 	)
