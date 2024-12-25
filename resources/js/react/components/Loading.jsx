802479a8a1222b944a5ac06412968b8371eefa11
Epsilonxaos <angelotr12@hotmail.com>
2024-12-25 15:45:07 -0600

    feat(Loading): :sparkles: Agregando componente de carga

---
+++ resources/js/react/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+	return (
+		<div className='fixed left-0 top-0 z-50 flex h-screen w-full items-center justify-center bg-grisClaro text-black'>
+			<span className='spinner'></span>
+		</div>
+	)
+}
+
+export default Loading
