91c575922aa8df7bea127e6bed2ec0a9127f6e89
Epsilonxaos <angelotr12@hotmail.com>
2024-12-25 15:47:56 -0600

    refactor(Text, Button): :recycle: Simplificando la estructura de componentes

--- resources/js/react/components/Text.tsx
+++ resources/js/react/components/Text.tsx
@@ -5,44 +5,18 @@ import type { TClassName } from '../types/main'
 
 import { cn } from '../utils/cn'
 
-const Text = ({
-	children,
-	className,
-	parseHtml,
-}: {
-	children: ReactNode
-	className?: TClassName
-	parseHtml?: boolean
-}) => {
+const Text = ({ children, className, parseHtml }: { children: ReactNode; className?: TClassName; parseHtml?: boolean }) => {
 	if (parseHtml && !children) return null
 
-	if (parseHtml)
-		return (
-			<div className={cn('mb-4 font-chassiS text-xs leading-[1.47] md:text-base', className)}>
-				{parse(children as string)}
-			</div>
-		)
+	if (parseHtml) return <div className={cn('mb-4 font-chassiS text-xs leading-[1.47] md:text-base', className)}>{parse(children as string)}</div>
 
 	return <p className={cn('mb-4 font-chassiS text-xs leading-[1.47] md:text-base', className)}>{children}</p>
 }
 
-const Title = ({
-	children,
-	className,
-	parseHtml,
-}: {
-	children: ReactNode | string
-	className?: TClassName
-	parseHtml?: boolean
-}) => {
+const Title = ({ children, className, parseHtml }: { children: ReactNode | string; className?: TClassName; parseHtml?: boolean }) => {
 	if (parseHtml && !children) return null
 
-	if (parseHtml)
-		return (
-			<div className={cn('mb-2 font-apercuPro text-base uppercase tracking-[4.8px]', className)}>
-				{parse(children as string)}
-			</div>
-		)
+	if (parseHtml) return <div className={cn('mb-2 font-apercuPro text-base uppercase tracking-[4.8px]', className)}>{parse(children as string)}</div>
 
 	return <h3 className={cn('mb-2 font-apercuPro text-base uppercase tracking-[4.8px]', className)}>{children}</h3>
 }
--- resources/js/react/components/buttons/Button.tsx
+++ resources/js/react/components/buttons/Button.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import type { TButton } from '../../types/main'
 
 import { cn } from '../../utils/cn'
@@ -5,9 +7,9 @@ import { cn } from '../../utils/cn'
 const Button = ({ children, onClick, type, className }: TButton) => {
 	return (
 		<button
-			type={type || 'button'}
+			type={type ?? 'button'}
 			className={cn(
-				'bg-crema2 border border-black px-5 py-2 font-apercuPro text-xs uppercase tracking-[1.2px] text-black transition-all hover:bg-black hover:text-white',
+				'border border-black bg-crema2 px-5 py-2 font-apercuPro text-xs uppercase tracking-[1.2px] text-black transition-all hover:bg-black hover:text-white',
 				className
 			)}
 			onClick={onClick}>
@@ -15,5 +17,37 @@ const Button = ({ children, onClick, type, className }: TButton) => {
 		</button>
 	)
 }
+
+const Url = ({ children, href, className }: TButton & { href: string }) => {
+	return (
+		<a
+			href={href}
+			target='_blank'
+			rel='noopener noreferrer'
+			className={cn(
+				'block border border-black bg-crema2 px-5 py-2 text-center font-apercuPro text-xs uppercase tracking-[1.2px] text-black transition-all hover:bg-black hover:text-white',
+				className
+			)}>
+			{children}
+		</a>
+	)
+}
+
+const Navigation = ({ to, children, onClick, className }: { to: string; children: React.ReactNode; onClick?: () => void; className?: string }) => {
+	return (
+		<Link
+			to={to}
+			className={cn(
+				'block border border-black bg-crema2 px-5 py-2 text-center font-apercuPro text-xs uppercase tracking-[1.2px] text-black transition-all hover:bg-black hover:text-white',
+				className
+			)}
+			onClick={onClick}>
+			{children}
+		</Link>
+	)
+}
+
+Button.Url = Url
+Button.Navigation = Navigation
 
 export default Button
