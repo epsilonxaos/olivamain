2d50edcdaae8bcba859659a2b142cb6d259e358f
Epsilonxaos <angelotr12@hotmail.com>
2024-12-25 15:46:26 -0600

    refactor(AppService): :recycle: Agregando servicios

--- resources/js/react/services/AppService.ts
+++ resources/js/react/services/AppService.ts
@@ -1,11 +1,22 @@
+import type { InitialDataResponse, Sucursal } from '../types/services/initial'
+
 import ApiRequest, { type ApiRequestOptions } from './ApiRequest'
 
 const apiRequest = new ApiRequest()
 
 export const getCargaInicialService = (opt?: ApiRequestOptions) => {
-	const requestOptions = {
+	const requestOptions: ApiRequestOptions = {
 		url: '/initial',
 	}
 
-	return apiRequest.fetchData({ ...requestOptions, ...opt })
+	return apiRequest.fetchData<InitialDataResponse>({ ...requestOptions, ...opt })
+}
+
+export const getSucursalService = (opt?: ApiRequestOptions<{ slug: string }>) => {
+	const requestOptions: ApiRequestOptions<{ slug: string }> = {
+		url: '/sucursal',
+		method: 'POST',
+	}
+
+	return apiRequest.fetchData<Sucursal>({ ...requestOptions, ...opt })
 }
