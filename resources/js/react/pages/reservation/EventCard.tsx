a210247a9b8cc48305b1e686b3b0e2713f832d67
Epsilonxaos <angelotr12@hotmail.com>
2024-12-28 13:21:21 -0600

    refactor(EventCard, Index): :recycle: Mejorando la gestión de reservas utilizando datos dinámicos de sucursales y actualizando el contenido de la interfaz

--- resources/js/react/pages/reservation/EventCard.tsx
+++ resources/js/react/pages/reservation/EventCard.tsx
@@ -1,83 +1,45 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+
+import { APP_URL } from '../../constants/constants'
 
 import ImgPlaceholder from '../../../../img/sucursal-banner-placeholder.png'
 import ImgPlaceholder2 from '../../../../img/sucursal-logo-2-placeholder.svg'
 import BorderCard from '../../components/BorderCard'
 import Text from '../../components/Text'
 import Button from '../../components/buttons/Button'
+import AppContext from '../../contexts/AppContext'
 
 const EventCard = () => {
+	const { state } = useContext(AppContext)
+	const { sucursals } = state
+
 	return (
 		<div className='grid grid-cols-1 md:gap-20'>
-			<BorderCard
-				classNameContainer='md:grid-cols-2'
-				classNameContainerImg='md:col-span-1'
-				classNameContainerText='md:col-span-1'
-				img={ImgPlaceholder}>
-				<div className='pt-6 md:p-6 md:text-center lg:p-10'>
-					<img
-						src={ImgPlaceholder2}
-						alt='Enoteca'
-						className='mx-auto mb-8 block w-full max-w-[160px]'
-					/>
-					<Text.Title>Centro Histórico de Mérida, Yuc.</Text.Title>
-					<Text>
-						Se encuentra en una <span className='italic'>antigua casa del Centro Histórico de Mérida Yucatán.</span>{' '}
-						Ofrece un menú de alta cocina Italiana en un ambiente chic industrial. Su lista extensa de más de 200
-						etiquetas de vinos ha sido seleccionada personalmente por el Chef para complementar el menú.
-					</Text>
+			{sucursals
+				.filter(su => !!su.reserva_iframe && !!su.activo_reservas)
+				.map(su => (
+					<BorderCard
+						key={'reserva-card-' + su.slug}
+						classNameContainer='md:grid-cols-2'
+						classNameContainerImg='md:col-span-1'
+						classNameContainerText='md:col-span-1'
+						img={APP_URL + su.cover_reservas}>
+						<div className='pt-6 md:p-6 md:text-center lg:p-10'>
+							<img
+								src={APP_URL + su.logo_2}
+								alt='Enoteca'
+								className='mx-auto mb-8 block w-full max-w-[160px]'
+							/>
+							<Text.Title>{su.ubicacion}</Text.Title>
+							<Text parseHtml>{su.descripcion_reservas}</Text>
 
-					<Link to={'/grupos-y-eventos/formulario'}>
-						<Button className=''>Reserva</Button>
-					</Link>
-				</div>
-			</BorderCard>
-			<BorderCard
-				classNameContainer='md:grid-cols-2'
-				classNameContainerImg='md:col-span-1'
-				classNameContainerText='md:col-span-1'
-				img={ImgPlaceholder}>
-				<div className='pt-6 md:p-6 md:text-center lg:p-10'>
-					<img
-						src={ImgPlaceholder2}
-						alt='Enoteca'
-						className='mx-auto mb-8 block w-full max-w-[160px]'
-					/>
-					<Text.Title>Centro Histórico de Mérida, Yuc.</Text.Title>
-					<Text>
-						Se encuentra en una <span className='italic'>antigua casa del Centro Histórico de Mérida Yucatán.</span>{' '}
-						Ofrece un menú de alta cocina Italiana en un ambiente chic industrial. Su lista extensa de más de 200
-						etiquetas de vinos ha sido seleccionada personalmente por el Chef para complementar el menú.
-					</Text>
-
-					<Link to={'/grupos-y-eventos/formulario'}>
-						<Button className=''>Reserva</Button>
-					</Link>
-				</div>
-			</BorderCard>
-			<BorderCard
-				classNameContainer='md:grid-cols-2'
-				classNameContainerImg='md:col-span-1'
-				classNameContainerText='md:col-span-1'
-				img={ImgPlaceholder}>
-				<div className='pt-6 md:p-6 md:text-center lg:p-10'>
-					<img
-						src={ImgPlaceholder2}
-						alt='Enoteca'
-						className='mx-auto mb-8 block w-full max-w-[160px]'
-					/>
-					<Text.Title>Centro Histórico de Mérida, Yuc.</Text.Title>
-					<Text>
-						Se encuentra en una <span className='italic'>antigua casa del Centro Histórico de Mérida Yucatán.</span>{' '}
-						Ofrece un menú de alta cocina Italiana en un ambiente chic industrial. Su lista extensa de más de 200
-						etiquetas de vinos ha sido seleccionada personalmente por el Chef para complementar el menú.
-					</Text>
-
-					<Link to={'/grupos-y-eventos/formulario'}>
-						<Button className=''>Reserva</Button>
-					</Link>
-				</div>
-			</BorderCard>
+							<Link to={'/grupos-y-eventos/formulario'}>
+								<Button className=''>Reserva</Button>
+							</Link>
+						</div>
+					</BorderCard>
+				))}
 		</div>
 	)
 }
--- resources/js/react/pages/reservation/Index.jsx
+++ resources/js/react/pages/reservation/Index.jsx
@@ -1,16 +1,30 @@
+import { useContext } from 'react'
+import { useTranslation } from 'react-i18next'
+
+import { APP_URL } from '../../constants/constants'
+
 import SectionUi from '../../components/SectionUI'
 import Text from '../../components/Text'
+import AppContext from '../../contexts/AppContext'
 import EventCard from './EventCard'
 
 const Index = () => {
+	const { i18n } = useTranslation()
+	const { language } = i18n
+	const { state } = useContext(AppContext)
+	const { website } = state
+
 	return (
 		<div className='bg-grisClaro'>
-			<SectionUi className='max-w-[1200px] px-0 xl:px-0'>
+			<SectionUi
+				className='max-w-[1200px] px-0 xl:px-0'
+				bg={APP_URL + website.reserva_cover}>
 				<header className='mx-auto max-w-[740px] px-8 pb-8 text-center md:pb-0 xl:px-24'>
-					<Text.Title className='mb-4'>¿En qué sucursal desea reservar?</Text.Title>
-					<Text className='mb-5'>
-						Nuestros restaurantes se especializan en cocina Italiana, fresca, autentica, y original. Todo es hecho a
-						mano dîa con día en nuestra cocina usando recetas tradicionales y originales.
+					<Text.Title className='mb-4'>{website[language].reserva_title}</Text.Title>
+					<Text
+						parseHtml
+						className='mb-5'>
+						{website[language].reserva_text}
 					</Text>
 				</header>
 
